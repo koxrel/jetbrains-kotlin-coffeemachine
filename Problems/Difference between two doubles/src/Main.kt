f895a431d1600fc28f5c2951ce8d8e7c1f3a9b76
koxrel <15571888+koxrel@users.noreply.github.com>
2020-09-05 19:30:40 +0300

    add float tasks

---
+++ Problems/Difference between two doubles/src/Main.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val firstNum = scanner.nextDouble()
+    val secondNum = scanner.nextDouble()
+
+    print(secondNum - firstNum)
+}
---
+++ Problems/Find X/src/Main.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val a = scanner.nextDouble()
+    val b = scanner.nextDouble()
+    val c = scanner.nextDouble()
+
+    print((c - b) / a)
+}
